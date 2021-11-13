66edbc7c05137cda37ef115764354c24767847bc
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-13 10:57:27 -0800

    Finished UI styling.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-setInterval(myTimer, 1000);
+/*
+Build a Clock
 
-function myTimer() {
-  const d = new Date();
-  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-}
+Step 1: Create a function called myTimer. 
+Step 2: Define a variable called todayDate and store in it a Date constructor that creates 
+a Date instance that represents the current date and time. To do this, leave the parameters of the
+Date constructor empty.
+Step 3: Select the div element from our HTML by it Id name. Using the innerHTML property, display in the div
+the todayDate with the toLocaleTimeString method applied to it. This methods turns the todayDate into a string.
+Step 4: Use the setInterval method to execute the myTimer function every 1000 milliseconds 
+(1000 milliseconds = 1 second).
+*/
+
+
+const myTimer = function() {
+  const todayDate = new Date();
+  document.getElementById("clock").innerHTML = todayDate.toLocaleTimeString();
+};
+
+setInterval(myTimer, 1000);
